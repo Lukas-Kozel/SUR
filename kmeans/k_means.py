1dcf2e0d905acc1dfce1c0de851f66d8d3204c2a
Lukas-kozel <l.kozel@outlook.cz>
2025-01-30 20:15:48 +0100

    generalized kmeans to n classes

--- kmeans/k_means.py
+++ kmeans/k_means.py
@@ -7,7 +7,7 @@ def compute_euclidean_distance(v1, v2):
 
 def compute_distance_to_each_point(reference, center_points)->np.array:
     distance_vector = []
-    for index, x in enumerate(center_points):   
+    for _, x in enumerate(center_points):   
             distance_vector.append(compute_euclidean_distance(x,reference))
     return np.array(distance_vector)
 
@@ -17,48 +17,61 @@ def find_closest_center(distance_vector):
     return min_index
 
 
-def classification(point_to_classify, center_points, class1, class2, class3):
+def classification(point_to_classify, center_points, class_list):
     distance_vector = compute_distance_to_each_point(point_to_classify,center_points)
     index_of_closest_center_point = find_closest_center(distance_vector)
 
-    if index_of_closest_center_point == 0:
-        class1.append_classification(point_to_classify)
-    if index_of_closest_center_point == 1:
-        class2.append_classification(point_to_classify)
-    if index_of_closest_center_point == 2:
-        class3.append_classification(point_to_classify)
+    class_list[index_of_closest_center_point].append_classification(point_to_classify)
 
 
-#return true if the algorithm should contiune otherwise return false
-def compute_new_center_points(class1,class2,class3)->bool:
-    class1_prev_center_point = class1.get_center_point()
-    class2_prev_center_point = class2.get_center_point()
-    class3_prev_center_point = class3.get_center_point()
-    class1.compute_center_point()
-    class2.compute_center_point()
-    class3.compute_center_point()
+def compute_new_center_points(class_list) -> bool:
+    """
+    Return True if at least one center changed
+    (meaning we should continue),
+    Return False if no center changed
+    (meaning we have converged).
+    """
+    any_changed = False
+    old_centers = []
 
-    return not (np.array_equal(class1_prev_center_point, class1.get_center_point()) and 
-                np.array_equal(class2_prev_center_point, class2.get_center_point()) and 
-                np.array_equal(class3_prev_center_point, class3.get_center_point()))
+    for c in class_list:
+        old_centers.append(c.get_center_point())
 
-def compute_whole_criterial_function(class1, class2, class3)->float:
-    class1.compute_criterial_function()
-    class2.compute_criterial_function()
-    class3.compute_criterial_function()
-    return class1.get_criterial_function()+class2.get_criterial_function()+class3.get_criterial_function()
+    for i, c in enumerate(class_list):
+        c.compute_center_point()
+        if not np.array_equal(old_centers[i], c.get_center_point()):
+            any_changed = True
 
-def plot_classes(class1, class2, class3, iteration):
+    return any_changed
+
+
+def compute_whole_criterial_function(class_list)->float:
+   cumulative_sum=0
+   for _,c in enumerate(class_list):
+       c.compute_criterial_function()
+       cumulative_sum+=c.get_criterial_function()
+   return cumulative_sum
+    
+def plot_classes(class_list, iteration):
+    
     plt.figure()
-    plt.scatter(*zip(*class1.get_classified_points()), color='red', label='Class 1')
-    plt.scatter(*zip(*class2.get_classified_points()), color='blue', label='Class 2')
-    plt.scatter(*zip(*class3.get_classified_points()), color='green', label='Class 3')
-    plt.scatter(*class1.get_center_point(), color='red', marker='x', s=100, label='Center 1')
-    plt.scatter(*class2.get_center_point(), color='blue', marker='x', s=100, label='Center 2')
-    plt.scatter(*class3.get_center_point(), color='green', marker='x', s=100, label='Center 3')
-    plt.title(f'Iteration {iteration}')
+    
+    colors = plt.cm.get_cmap("tab10", len(class_list))
+    
+    for i, c in enumerate(class_list):
+        classified_points = c.get_classified_points()
+        center_point = c.get_center_point()
+        
+        if len(classified_points) > 0:
+            xs, ys = zip(*classified_points)
+            plt.scatter(xs, ys, color=colors(i), label=f'Class {i}')
+        
+        plt.scatter(center_point[0], center_point[1],color=colors(i), marker='x', s=100, label=f'Center {i}')
+    
+    plt.title(f"Iteration {iteration}")
     plt.legend()
     plt.show()
+
 
 def plot_criterial_function(history_of_criterial_function):
     plt.figure()
@@ -70,39 +83,45 @@ def plot_criterial_function(history_of_criterial_function):
     plt.show()
 
 def main():
+
+    # setup
     training_set = np.array([[4, -7], [-1, -3], [-3,4], [-4,6],[2,-3], [4,-6], [-3,1], [-1,2], [6,-9], [-1,-1]])
-    center_points = np.array([training_set[0], training_set[5], training_set[8]])
-    class1 = Class(1)
-    class1.set_center_point(center_points[0])
-    class2 = Class(2)
-    class2.set_center_point(center_points[1])
-    class3 = Class(3)
-    class3.set_center_point(center_points[2])
+    number_of_classes = 4
+    center_points = np.array([training_set[1], training_set[3], training_set[9], training_set[2]])
+
+    assert number_of_classes == len(center_points), f"Error: number_of_classes ({number_of_classes}) does not match the number of center points ({len(center_points)})"
+
+    class_list =[]
+
+    for class_index in range(number_of_classes):
+        class_list.append(Class(class_index))
+        class_list[-1].set_center_point(center_points[class_index])
+
     history_of_criterial_function = []
     iteration = 0
     while True:
         iteration += 1
-        center_points = np.array([class1.get_center_point(),class2.get_center_point(),class3.get_center_point()])
-        
-        class1.set_of_points = []
-        class2.set_of_points = []
-        class3.set_of_points = []
-        
+        center_points=[]
+        for _,c in enumerate(class_list):
+            center_points.append(c.get_center_point())
+            c.set_of_points = []
+            
         for vect in training_set:
-            classification(vect,center_points, class1, class2, class3)
-        
+            classification(vect,center_points, class_list)
+
         print(f'Iteration {iteration}:')
-        print(f'Class 1: {class1.get_classified_points()}')
-        print(f'Class 2: {class2.get_classified_points()}')
-        print(f'Class 3: {class3.get_classified_points()}')
+        for i,c in enumerate(class_list):
+            print(f'Class {i}: {c.get_classified_points()}')
+                
+        plot_classes(class_list, iteration)
         
-        plot_classes(class1, class2, class3, iteration)
-        current_criterial_value = compute_whole_criterial_function(class1, class2, class3)
+        current_criterial_value = compute_whole_criterial_function(class_list)
         history_of_criterial_function.append(current_criterial_value)
-        if not compute_new_center_points(class1, class2, class3):
+
+        if not compute_new_center_points(class_list):
             break
 
-    plot_classes(class1, class2, class3, "final division")
+    plot_classes(class_list, "final division")
     plot_criterial_function(history_of_criterial_function)
 
 if __name__ == "__main__":
